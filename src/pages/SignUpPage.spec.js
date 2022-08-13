a0919d28a0e2262219e29dbf22c9bf17fb2e6e85
repon <repon27@yahoo.co.jp>
2022-08-13 19:20:35 +0900

    API呼び出しテスト途中

---
+++ src/pages/SignUpPage.spec.js
@@ -0,0 +1,98 @@
+import SignUpPage from './SignUpPage.vue'
+import { describe, it, expect, afterEach, vi } from 'vitest'
+import { render, screen, cleanup, fireEvent } from '@testing-library/vue'
+
+describe('SignUpPage', () => {
+  afterEach(cleanup)
+
+  describe('レイアウト', () => {
+    it('Sign Upヘッダーが表示される', () => {
+      render(SignUpPage)
+      const header = screen.getByRole('heading', { name: 'Sign Up' })
+      expect(header).toBeTruthy()
+    })
+
+    it('ユーザー名の入力フォームが表示される', () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('ユーザー名')
+      expect(input).toBeTruthy()
+    })
+
+    it('Eメールの入力フォームが表示される', () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('メールアドレス')
+      expect(input).toBeTruthy()
+    })
+
+    it('パスワードが表示される', () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('パスワード')
+      expect(input).toBeTruthy()
+    })
+
+    it('パスワードの確認が表示される', () => {
+      render(SignUpPage)
+      const input = screen.queryByLabelText('パスワードの確認')
+      expect(input).toBeTruthy()
+    })
+
+    it('パスワードの入力フォームのtypeがpasswordであること', () => {
+      render(SignUpPage)
+      const input = screen.getByLabelText('パスワード')
+      expect(input.type).toBe('password')
+    })
+
+    it('パスワードの確認の入力フォームのtypeがpasswordであること', () => {
+      render(SignUpPage)
+      const input = screen.getByLabelText('パスワードの確認')
+      expect(input.type).toBe('password')
+    })
+
+    it('登録用のボタンが表示される', () => {
+      render(SignUpPage)
+      const button = screen.getByRole('button', { name: '登録' })
+      expect(button).toBeTruthy()
+    })
+
+    it('登録ボタンが初期表示時はdisableとなっている', () => {
+      render(SignUpPage)
+      const button = screen.getByRole('button', { name: '登録' })
+      expect(button.disabled).toBeTruthy()
+    })
+  })
+
+  describe('インタラクション', () => {
+    it('全フォームを入力済み、かつパスワードとパスワード確認が同じ値の場合、登録のdisabledが解除される', async () => {
+      render(SignUpPage)
+      await fillAllForm('Usern', 'user@example.com', 'P4ssw0rd', 'P4ssw0rd')
+      const button = screen.getByRole('button', { name: '登録' })
+      expect(button.disabled).toBe(false)
+    })
+
+    it('全フォームを入力済みでも、パスワードとパスワードの確認が不一致の場合、登録ボタンがdisabledになる', async () => {
+      render(SignUpPage)
+      await fillAllForm('Usern', 'user@example.com', 'P4ssw0rd', 'P5ssw0rd')
+      const button = screen.getByRole('button', { name: '登録' })
+      expect(button.disabled).toBe(true)
+    })
+
+    async function fillAllForm(username, email, password, confirmPassword) {
+      const usernameInput = screen.getByLabelText('ユーザー名')
+      const emailInput = screen.getByLabelText('メールアドレス')
+      const passwordInput = screen.getByLabelText('パスワード')
+      const confirmPasswordInput = screen.getByLabelText('パスワードの確認')
+      await fireEvent.update(usernameInput, username)
+      await fireEvent.update(emailInput, email)
+      await fireEvent.update(passwordInput, password)
+      await fireEvent.update(confirmPasswordInput, confirmPassword)
+    }
+
+    it('登録ボタン押下時にユーザー名、メールアドレス、パスワードをサーバに送信する', async () => {
+      render(SignUpPage)
+      await fillAllForm('Usern', 'user@example.com', 'P4ssw0rd', 'P4ssw0rd')
+      const button = screen.getByRole('button', { name: '登録' })
+
+      const mockFn = vi.fn()
+    })
+  })
+})
---
+++ src/pages/SignUpPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Sign Up</h1>
+  <label for="username">ユーザー名</label>
+  <input id="username" type="text" v-model="username" />
+  <label for="email">メールアドレス</label>
+  <input id="email" type="text" v-model="email">
+  <label for="password">パスワード</label>
+  <input id="password" type="password" v-model="password" />
+  <label for="confirm_password">パスワードの確認</label>
+  <input id="confirm_password" type="password" v-model="confirm_password" />
+  <button :disabled="isDisabled">登録</button>
+</template>
+
+<script>
+export default {
+  name: 'SignUpPage',
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      confirm_password: '',
+    }
+  },
+  computed: {
+    isDisabled() {
+      if (
+        this.username &&
+        this.email &&
+        this.password &&
+        this.password === this.confirm_password
+      )
+        return false
+      return true
+    }
+  }
+}
+</script>
