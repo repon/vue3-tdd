899d511abfd40ccfdcbc56d916fc53317c126ea1
repon <repon27@yahoo.co.jp>
2022-08-14 18:18:53 +0900

    done

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,111 +1,129 @@
-import SignUpPage from './SignUpPage.vue'
-import { describe, it, expect, afterEach, vi } from 'vitest'
-import { render, screen, cleanup, fireEvent } from '@testing-library/vue'
-import axios from 'axios'
+import SignUpPage from "./SignUpPage.vue";
+import { describe, it, expect, afterEach, beforeEach } from "vitest";
+import { render, screen, cleanup, fireEvent } from "@testing-library/vue";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
 
-describe('SignUpPage', () => {
-  afterEach(cleanup)
-
-  describe('レイアウト', () => {
-    it('Sign Upヘッダーが表示される', () => {
-      render(SignUpPage)
-      const header = screen.getByRole('heading', { name: 'Sign Up' })
-      expect(header).toBeTruthy()
-    })
-
-    it('ユーザー名の入力フォームが表示される', () => {
-      render(SignUpPage)
-      const input = screen.queryByLabelText('ユーザー名')
-      expect(input).toBeTruthy()
-    })
-
-    it('Eメールの入力フォームが表示される', () => {
-      render(SignUpPage)
-      const input = screen.queryByLabelText('メールアドレス')
-      expect(input).toBeTruthy()
-    })
-
-    it('パスワードが表示される', () => {
-      render(SignUpPage)
-      const input = screen.queryByLabelText('パスワード')
-      expect(input).toBeTruthy()
-    })
-
-    it('パスワードの確認が表示される', () => {
-      render(SignUpPage)
-      const input = screen.queryByLabelText('パスワードの確認')
-      expect(input).toBeTruthy()
-    })
-
-    it('パスワードの入力フォームのtypeがpasswordであること', () => {
-      render(SignUpPage)
-      const input = screen.getByLabelText('パスワード')
-      expect(input.type).toBe('password')
-    })
-
-    it('パスワードの確認の入力フォームのtypeがpasswordであること', () => {
-      render(SignUpPage)
-      const input = screen.getByLabelText('パスワードの確認')
-      expect(input.type).toBe('password')
-    })
-
-    it('登録用のボタンが表示される', () => {
-      render(SignUpPage)
-      const button = screen.getByRole('button', { name: '登録' })
-      expect(button).toBeTruthy()
-    })
-
-    it('登録ボタンが初期表示時はdisableとなっている', () => {
-      render(SignUpPage)
-      const button = screen.getByRole('button', { name: '登録' })
-      expect(button.disabled).toBeTruthy()
-    })
+describe("SignUpPage", () => {
+  beforeEach(() => {
+    render(SignUpPage)
   })
+  afterEach(cleanup);
 
-  describe('インタラクション', () => {
-    it('全フォームを入力済み、かつパスワードとパスワード確認が同じ値の場合、登録のdisabledが解除される', async () => {
-      render(SignUpPage)
-      await fillAllForm('Usern', 'user@example.com', 'P4ssw0rd', 'P4ssw0rd')
-      const button = screen.getByRole('button', { name: '登録' })
-      expect(button.disabled).toBe(false)
-    })
+  describe("レイアウト", () => {
+    it("Sign Upヘッダーが表示される", () => {
+      const header = screen.getByRole("heading", { name: "Sign Up" });
+      expect(header).toBeTruthy();
+    });
 
-    it('全フォームを入力済みでも、パスワードとパスワードの確認が不一致の場合、登録ボタンがdisabledになる', async () => {
-      render(SignUpPage)
-      await fillAllForm('Usern', 'user@example.com', 'P4ssw0rd', 'P5ssw0rd')
-      const button = screen.getByRole('button', { name: '登録' })
-      expect(button.disabled).toBe(true)
-    })
+    it("ユーザー名の入力フォームが表示される", () => {
+      const input = screen.queryByLabelText("ユーザー名");
+      expect(input).toBeTruthy();
+    });
 
-    async function fillAllForm(username, email, password, confirmPassword) {
-      const usernameInput = screen.getByLabelText('ユーザー名')
-      const emailInput = screen.getByLabelText('メールアドレス')
-      const passwordInput = screen.getByLabelText('パスワード')
-      const confirmPasswordInput = screen.getByLabelText('パスワードの確認')
-      await fireEvent.update(usernameInput, username)
-      await fireEvent.update(emailInput, email)
-      await fireEvent.update(passwordInput, password)
-      await fireEvent.update(confirmPasswordInput, confirmPassword)
+    it("Eメールの入力フォームが表示される", () => {
+      const input = screen.queryByLabelText("メールアドレス");
+      expect(input).toBeTruthy();
+    });
+
+    it("パスワードが表示される", () => {
+      const input = screen.queryByLabelText("パスワード");
+      expect(input).toBeTruthy();
+    });
+
+    it("パスワードの確認が表示される", () => {
+      const input = screen.queryByLabelText("パスワードの確認");
+      expect(input).toBeTruthy();
+    });
+
+    it("パスワードの入力フォームのtypeがpasswordであること", () => {
+      const input = screen.getByLabelText("パスワード");
+      expect(input.type).toBe("password");
+    });
+
+    it("パスワードの確認の入力フォームのtypeがpasswordであること", () => {
+      const input = screen.getByLabelText("パスワードの確認");
+      expect(input.type).toBe("password");
+    });
+
+    it("登録用のボタンが表示される", () => {
+      const button = screen.getByRole("button", { name: "登録" });
+      expect(button).toBeTruthy();
+    });
+
+    it("登録ボタンが初期表示時はdisableとなっている", () => {
+      const button = screen.getByRole("button", { name: "登録" });
+      expect(button.disabled).toBeTruthy();
+    });
+  });
+
+  describe("インタラクション", () => {
+    // モックサーバー準備
+    let requestBody;
+    const server = setupServer(
+      rest.post("/api/v1/users", async (req, res, ctx) => {
+        requestBody = await req.json();
+        if (requestBody.username === "Error1") {
+          return res(
+            ctx.status(500),
+            ctx.json({
+              error: {
+                message: "サーバーエラーです。時間をおいて試してください",
+              },
+            })
+          );
+        }
+        return res(ctx.status(200));
+      })
+    );
+
+    async function responseServerCheck(username) {
+      server.listen();
+      await fillAllForm(username, "user@example.com", "P4ssw0rd", "P4ssw0rd");
+      const button = screen.getByRole("button", { name: "登録" });
+      await fireEvent.click(button);
+      await server.close();
     }
 
-    it('登録ボタン押下時にユーザー名、メールアドレス、パスワードをサーバに送信する', async () => {
-      render(SignUpPage)
-      await fillAllForm('Usern', 'user@example.com', 'P4ssw0rd', 'P4ssw0rd')
-      const button = screen.getByRole('button', { name: '登録' })
+    it("全フォームを入力済み、かつパスワードとパスワード確認が同じ値の場合、登録のdisabledが解除される", async () => {
+      await fillAllForm("Usern", "user@example.com", "P4ssw0rd", "P4ssw0rd");
+      const button = screen.getByRole("button", { name: "登録" });
+      expect(button.disabled).toBe(false);
+    });
 
-      const mockFn = vi.fn()
-      axios.post = mockFn
+    it("全フォームを入力済みでも、パスワードとパスワードの確認が不一致の場合、登録ボタンがdisabledになる", async () => {
+      await fillAllForm("Usern", "user@example.com", "P4ssw0rd", "P5ssw0rd");
+      const button = screen.getByRole("button", { name: "登録" });
+      expect(button.disabled).toBe(true);
+    });
 
-      await fireEvent.click(button)
+    async function fillAllForm(username, email, password, confirmPassword) {
+      const usernameInput = screen.getByLabelText("ユーザー名");
+      const emailInput = screen.getByLabelText("メールアドレス");
+      const passwordInput = screen.getByLabelText("パスワード");
+      const confirmPasswordInput = screen.getByLabelText("パスワードの確認");
+      await fireEvent.update(usernameInput, username);
+      await fireEvent.update(emailInput, email);
+      await fireEvent.update(passwordInput, password);
+      await fireEvent.update(confirmPasswordInput, confirmPassword);
+    }
 
-      const firstCall = mockFn.mock.calls[0]
-      const body = firstCall[1]
+    it("登録ボタン押下時にユーザー名、メールアドレス、パスワードをサーバに送信する", async () => {
+      await responseServerCheck("Usern");
+      expect(requestBody).toEqual({
+        username: "Usern",
+        email: "user@example.com",
+        password: "P4ssw0rd",
+      });
+    });
 
-      expect(body).toEqual({
-        username: 'Usern',
-        email: 'user@example.com',
-        password: 'P4ssw0rd',
-      })
-    })
-  })
-})
+    it("登録時にサーバーからエラーが返された場合、エラーメッセージを表示する", async () => {
+      await responseServerCheck("Error1");
+
+      const text = await screen.findByText(
+        "サーバーエラーです。時間をおいて試してください"
+      );
+      expect(text).toBeTruthy();
+    });
+  });
+});
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,28 +1,32 @@
 <template>
-  <h1>Sign Up</h1>
-  <label for="username">ユーザー名</label>
-  <input id="username" type="text" v-model="username" />
-  <label for="email">メールアドレス</label>
-  <input id="email" type="text" v-model="email">
-  <label for="password">パスワード</label>
-  <input id="password" type="password" v-model="password" />
-  <label for="confirm_password">パスワードの確認</label>
-  <input id="confirm_password" type="password" v-model="confirm_password" />
-  <button :disabled="isDisabled" @click="submit">登録</button>
+  <form>
+    <h1>Sign Up</h1>
+    <label for="username">ユーザー名</label>
+    <input id="username" type="text" v-model="username" />
+    <label for="email">メールアドレス</label>
+    <input id="email" type="text" v-model="email" />
+    <label for="password">パスワード</label>
+    <input id="password" type="password" v-model="password" />
+    <label for="confirm_password">パスワードの確認</label>
+    <input id="confirm_password" type="password" v-model="confirm_password" />
+    <button :disabled="isDisabled" @click.prevent="submit">登録</button>
+    <p v-if="errorMessage">{{ errorMessage }}</p>
+  </form>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  name: 'SignUpPage',
+  name: "SignUpPage",
   data() {
     return {
-      username: '',
-      email: '',
-      password: '',
-      confirm_password: '',
-    }
+      username: "",
+      email: "",
+      password: "",
+      confirm_password: "",
+      errorMessage: "",
+    };
   },
   computed: {
     isDisabled() {
@@ -32,18 +36,22 @@ export default {
         this.password &&
         this.password === this.confirm_password
       )
-        return false
-      return true
-    }
+        return false;
+      return true;
+    },
   },
   methods: {
-    submit() {
-      axios.post('/api/v1/users', {
-        username: this.username,
-        email: this.email,
-        password: this.password,
-      })
-    }
-  }
-}
+    async submit() {
+      try {
+        await axios.post("/api/v1/users", {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+        });
+      } catch (error) {
+        this.errorMessage = error.response.data.error.message;
+      }
+    },
+  },
+};
 </script>
